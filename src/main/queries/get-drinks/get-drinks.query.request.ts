90be7829a1e8c20823e3ece3951008b50bf9d5ec
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 17:04:23 -0300

    docs: add swagger documentation

--- src/main/queries/get-drinks/get-drinks.query.request.ts
+++ src/main/queries/get-drinks/get-drinks.query.request.ts
@@ -1,11 +1,40 @@
+import {ApiProperty, ApiPropertyOptional} from "@nestjs/swagger";
+
 export class GetDrinksQueryRequest {
   constructor(
-    public name?: string,
-    public category?: string,
-    public preparation?: string,
-    public sort?: 'asc' | 'desc',
-    public number?: number,
-    public offset?: number
+    name?: string,
+    category?: string,
+    preparation?: string,
+    sort?: 'asc' | 'desc',
+    number?: number,
+    offset?: number
   ) {
+    this.name = name
+    this.category = category
+    this.preparation = preparation
+    this.sort = sort
+    this.number = number
+    this.offset = offset
   }
+
+  @ApiPropertyOptional({ example: 'Wine', description: 'The name of the drink' })
+  public name?: string
+
+  @ApiPropertyOptional({ example: 'Wine', description: 'The category of the drink' })
+  public category?: string
+
+  @ApiPropertyOptional({
+    example: 'Remove the cover and fill the cup',
+    description: 'The preparation method'
+  })
+  public preparation?: string
+
+  @ApiPropertyOptional({ example: 'asc', description: 'The sort direction' })
+  public sort?: 'asc' | 'desc'
+
+  @ApiPropertyOptional({ example: 10, description: 'The number of records to be returned' })
+  public number?: number
+
+  @ApiPropertyOptional({ example: 1, description: 'The page number' })
+  public offset?: number
 }
