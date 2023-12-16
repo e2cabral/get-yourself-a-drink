0176c22369119c3e38fb35f1139b3e62f2588443
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 17:04:17 -0300

    docs: add swagger documentation

--- src/presentation/controllers/drinks/drinks.controller.ts
+++ src/presentation/controllers/drinks/drinks.controller.ts
@@ -1,12 +1,20 @@
 import {Controller, Get, Query} from '@nestjs/common';
 import {GetDrinksQueryRequest} from "../../../main/queries/get-drinks/get-drinks.query.request";
 import {DrinksService} from "../../../domain/services/drinks/drinks.service";
+import {ApiQuery, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {Drink} from "../../../domain/models/drink.model";
 
 @Controller('drinks')
 export class DrinksController {
   constructor(private service: DrinksService) {
   }
   @Get('/')
+  @ApiTags('Drinks')
+  @ApiResponse({
+    status: 200,
+    description: 'The random recipe has been successfully retrieved.',
+    type: [Drink],
+  })
   async getDrinks(@Query() query: GetDrinksQueryRequest) {
     try {
       return this.service.getDrinks(query)
