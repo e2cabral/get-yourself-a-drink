4f4ecda7ccf9b0b2f7a42dc5a72679abe8582232
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 12:56:06 -0300

    refactor: add more properties to increase the possibilities of filtering

--- src/main/queries/get-drinks/get-drinks.query.request.ts
+++ src/main/queries/get-drinks/get-drinks.query.request.ts
@@ -2,7 +2,10 @@ export class GetDrinksQueryRequest {
   constructor(
     public name: string,
     public category: string,
-    public preparation: string
+    public preparation: string,
+    public sort: 'asc' | 'desc',
+    public number: number,
+    public offset: number
   ) {
   }
 }
