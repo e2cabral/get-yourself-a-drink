3d1e81876eb8bfd922af6972b34cfe683990ffb0
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 13:05:24 -0300

    chore: add a get drinks method

--- src/domain/services/drinks/drinks.service.ts
+++ src/domain/services/drinks/drinks.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import {GetDrinksQueryRequest} from "../../../main/queries/get-drinks/get-drinks.query.request";
+import {QueryBus} from "@nestjs/cqrs";
 
 @Injectable()
-export class DrinksService {}
+export class DrinksService {
+  constructor(private queryBus: QueryBus) {
+  }
+  async getDrinks(query: GetDrinksQueryRequest) {
+    try {
+      const {
+        name, category, preparation,
+        sort, offset, number
+      } = query
+
+      return this.queryBus.execute(
+        new GetDrinksQueryRequest(
+          name, category, preparation,
+          sort, number, offset
+        )
+      )
+    } catch (err) {
+      throw err
+    }
+  }
+}
