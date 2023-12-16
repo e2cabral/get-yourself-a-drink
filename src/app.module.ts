c584005c2978c59218ce7b3b4e69014c071b3e70
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 11:54:25 -0300

    chore: create base modules of the architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PresentationModule } from './presentation/presentation.module';
+import { MainModule } from './main/main.module';
+import { DomainModule } from './domain/domain.module';
 
 @Module({
-  imports: [PresentationModule],
+  imports: [PresentationModule, MainModule, DomainModule],
   controllers: [],
   providers: [],
 })
---
+++ src/domain/domain.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { DrinksService } from './services/drinks/drinks.service';
+
+@Module({
+  providers: []
+})
+export class DomainModule {}
