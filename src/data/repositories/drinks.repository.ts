9b7ff32f81785848af4ca7656e7cb0caa95e6c9a
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 15:28:58 -0300

    feat: make a simple query to the database

--- src/data/repositories/drinks.repository.ts
+++ src/data/repositories/drinks.repository.ts
@@ -1,10 +1,30 @@
-import {Injectable} from "@nestjs/common";
+import {Inject, Injectable} from "@nestjs/common";
 import {Drink} from "../../domain/models/drink.model";
 import {GetDrinksQueryRequest} from "../../main/queries/get-drinks/get-drinks.query.request";
+import {Like, Repository} from "typeorm";
+import {DrinkEntity} from "../../infra/database/entity/drink.entity";
 
 @Injectable()
 export class DrinksRepository {
+  constructor(@Inject('DRINKS_REPOSITORY') private repository: Repository<DrinkEntity>) {
+  }
   async getDrinks(query: GetDrinksQueryRequest): Promise<Array<Drink>> {
-    return []
+    try {
+      const { name, category, preparation } = query
+      return this
+        .repository
+        .find({
+          where: {
+            name: Like(`%${name}%`),
+            preparation: Like(`%${preparation}%`),
+            category,
+          },
+          skip: (query.offset - 1) * query.number,
+          take: query.number,
+          order: { name: query.sort }
+        })
+    } catch (err) {
+      throw err
+    }
   }
 }
