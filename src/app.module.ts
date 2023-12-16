c4bf7bed873ee1e28e654e75f41b214959535c4f
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 12:14:29 -0300

    chore: create a data module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PresentationModule } from './presentation/presentation.module';
 import { MainModule } from './main/main.module';
 import { DomainModule } from './domain/domain.module';
+import { DataModule } from './data/data.module';
 
 @Module({
-  imports: [PresentationModule, MainModule, DomainModule],
+  imports: [PresentationModule, MainModule, DomainModule, DataModule],
   controllers: [],
   providers: [],
 })
