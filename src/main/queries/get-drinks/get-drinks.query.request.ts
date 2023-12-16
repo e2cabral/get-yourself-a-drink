386b911a3edb0f01801b1c1e0ff312667369a1ed
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 15:28:25 -0300

    chore: turn all properties into nullable

--- src/main/queries/get-drinks/get-drinks.query.request.ts
+++ src/main/queries/get-drinks/get-drinks.query.request.ts
@@ -1,11 +1,11 @@
 export class GetDrinksQueryRequest {
   constructor(
-    public name: string,
-    public category: string,
-    public preparation: string,
-    public sort: 'asc' | 'desc',
-    public number: number,
-    public offset: number
+    public name?: string,
+    public category?: string,
+    public preparation?: string,
+    public sort?: 'asc' | 'desc',
+    public number?: number,
+    public offset?: number
   ) {
   }
 }
