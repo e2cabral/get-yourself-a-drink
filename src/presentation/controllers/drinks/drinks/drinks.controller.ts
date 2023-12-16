0ee9244c3c0eb0ea47538d0c0ef7d0414ef82aa6
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 13:04:16 -0300

    chore: create a drinks controller

---
+++ src/presentation/controllers/drinks/drinks/drinks.controller.ts
@@ -0,0 +1,17 @@
+import {Controller, Get, Query} from '@nestjs/common';
+import {GetDrinksQueryRequest} from "../../../../main/queries/get-drinks/get-drinks.query.request";
+import {DrinksService} from "../../../../domain/services/drinks/drinks.service";
+
+@Controller('drinks')
+export class DrinksController {
+  constructor(private service: DrinksService) {
+  }
+  @Get('/')
+  async getDrinks(@Query() query: GetDrinksQueryRequest) {
+    try {
+      return this.service.getDrinks(query)
+    } catch (err) {
+
+    }
+  }
+}
