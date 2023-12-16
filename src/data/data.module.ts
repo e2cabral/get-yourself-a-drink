776fa582e90e762aeae06d0c114b9adc43c24694
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 15:24:57 -0300

    chore: import the infra module into the data module

--- src/data/data.module.ts
+++ src/data/data.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import {DrinksRepository} from "./repositories/drinks.repository";
+import {InfraModule} from "../infra/infra.module";
 
 @Module({
+  imports: [InfraModule],
   providers: [DrinksRepository],
   exports: [DrinksRepository]
 })
