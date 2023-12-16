6599797a2b25a7eb9e93529fa3783a8d21a5b571
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 12:58:16 -0300

    chore: implements the repository into the query handler

--- src/main/queries/get-drinks/get-drinks.handler.ts
+++ src/main/queries/get-drinks/get-drinks.handler.ts
@@ -1,10 +1,19 @@
 import {IQueryHandler, QueryHandler} from "@nestjs/cqrs";
 import {GetDrinksQueryRequest} from "./get-drinks.query.request";
+import {DrinksRepository} from "../../../data/repositories/drinks.repository";
+import {GetDrinksQueryResponse} from "./get-drinks.query.response";
 
 @QueryHandler(GetDrinksQueryRequest)
 export class GetDrinksHandler implements IQueryHandler<GetDrinksQueryRequest> {
-  execute(query: GetDrinksQueryRequest): Promise<any> {
-    return Promise.resolve(undefined);
+  constructor(private repository: DrinksRepository) {
+  }
+  async execute(query: GetDrinksQueryRequest): Promise<GetDrinksQueryResponse> {
+    try {
+      const drinks = await this.repository.getDrinks(query)
+      return GetDrinksQueryResponse(drinks)
+    } catch (err) {
+      throw err
+    }
   }
   
 }
