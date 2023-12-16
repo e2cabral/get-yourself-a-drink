74fc3ea55ed0f35be36b61488ac964d12dd4d9af
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 17:31:39 -0300

    chore: add the handler to the main module

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import {CqrsModule, QueryBus} from "@nestjs/cqrs";
 import {GetDrinksHandler} from "./queries/get-drinks/get-drinks.handler";
 import {DataModule} from "../data/data.module";
+import {GetDrinkByIdHandler} from "./queries/get-drink-by-id/get-drink-by-id.handler";
 
 @Module({
   imports: [CqrsModule, DataModule],
-  providers: [GetDrinksHandler]
+  providers: [GetDrinksHandler, GetDrinkByIdHandler]
 })
 export class MainModule {}
