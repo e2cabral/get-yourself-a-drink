8ee15a4f9e356aa61a176f7bec288a65b07585c0
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-16 13:04:59 -0300

    chore: add the controller to the presentation module

--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { HealthcheckController } from './controllers/common/healthcheck.controller';
 import {TerminusModule} from "@nestjs/terminus";
 import {HttpModule} from "@nestjs/axios";
+import { DrinksController } from './controllers/drinks/drinks/drinks.controller';
 
 @Module({
   imports: [
     TerminusModule,
     HttpModule
   ],
-  controllers: [HealthcheckController]
+  controllers: [HealthcheckController, DrinksController]
 })
 export class PresentationModule {}
