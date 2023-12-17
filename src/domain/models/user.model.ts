45992e3f6c03e51fda1b3f762df87bdb093211fd
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-17 14:21:10 -0300

    chore: create a user model

---
+++ src/domain/models/user.model.ts
@@ -0,0 +1,16 @@
+import {Drink} from "./drink.model";
+
+export class User {
+  constructor(
+    id: number,
+    name: string,
+    drinks: Array<Drink>
+  ) {
+    this.id = id
+    this.name = name
+    this.drinks = drinks
+  }
+  id: number
+  name: string
+  drinks: Array<Drink>
+}
