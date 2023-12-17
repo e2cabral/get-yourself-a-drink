7741f1470e01e8dc73f6ad3f75365b5e86c45f28
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-17 17:14:53 -0300

    chore: create a favorite repository provider

--- src/infra/database/providers/drinks.repository.provider.ts
+++ src/infra/database/providers/drinks.repository.provider.ts
@@ -1,6 +1,7 @@
 import {DataSource} from "typeorm";
 import {DrinkEntity} from "../entity/drink.entity";
 import {UserEntity} from "../entity/user.entity";
+import {FavoriteEntity} from "../entity/favorite.entity";
 
 export const DrinksRepositoryProvider = () => ({
   provide: 'DRINKS_REPOSITORY',
@@ -11,5 +12,11 @@ export const DrinksRepositoryProvider = () => ({
 export const UserRepositoryProvider = () => ({
   provide: 'USERS_REPOSITORY',
   useFactory: (dataSource: DataSource) => dataSource.getRepository(UserEntity),
+  inject: ['POSTGRES_DATA_SOURCE']
+})
+
+export const FavoriteRepositoryProvider = () => ({
+  provide: 'FAVORITE_REPOSITORY',
+  useFactory: (dataSource: DataSource) => dataSource.getRepository(FavoriteEntity),
   inject: ['POSTGRES_DATA_SOURCE']
 })
