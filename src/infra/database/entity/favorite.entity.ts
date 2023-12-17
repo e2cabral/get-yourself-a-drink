0c178df533e56e2f55b826c4453235397cf0e85f
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-17 17:14:02 -0300

    chore: create a favorite entity

---
+++ src/infra/database/entity/favorite.entity.ts
@@ -0,0 +1,20 @@
+import {Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn,} from "typeorm";
+import {DrinkEntity} from "./drink.entity";
+import {UserEntity} from "./user.entity";
+
+@Entity({ name: 'favorites', schema: 'favorites' })
+export class FavoriteEntity {
+  @PrimaryColumn({ name: 'user_id' })
+  userId: number
+
+  @PrimaryColumn({ name: 'drink_id' })
+  drinkId: number
+
+  @ManyToOne(() => UserEntity)
+  @JoinColumn({ name: 'user_id' })
+  users: Array<UserEntity>
+
+  @ManyToOne(() => DrinkEntity)
+  @JoinColumn({ name: 'drink_id' })
+  drinks: Array<DrinkEntity>
+}
