225be67532bf11779f99fd6c9ff5e433dd1b4b3a
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-17 17:27:42 -0300

    docs: add swagger documentation

--- src/domain/models/favorite.model.ts
+++ src/domain/models/favorite.model.ts
@@ -1,5 +1,6 @@
 import {User} from "./user.model";
 import {Drink} from "./drink.model";
+import {ApiProperty} from "@nestjs/swagger";
 
 export class Favorite {
   constructor(
@@ -13,7 +14,11 @@ export class Favorite {
     this.users = users
     this.drinks = drinks
   }
+
+  @ApiProperty({ example: 1, description: 'The unique id of the user' })
   userId: number
+
+  @ApiProperty({ example: 1, description: 'The unique id of the drink' })
   drinkId: number
   users?: Array<User>
   drinks?: Array<Drink>
--- src/presentation/controllers/favorites/favorites.controller.ts
+++ src/presentation/controllers/favorites/favorites.controller.ts
@@ -2,12 +2,20 @@ import {Controller, Param, Post} from '@nestjs/common';
 import {FavoritesService} from "../../../domain/services/favorites/favorites.service";
 import {ResponseFactory} from "../../../infra/factory/response.factory";
 import {InternalServerError} from "../../../infra/http/errors";
+import {ApiResponse, ApiTags} from "@nestjs/swagger";
+import {Drink} from "../../../domain/models/drink.model";
+import {Favorite} from "../../../domain/models/favorite.model";
 
 @Controller('favorite')
 export class FavoritesController {
-  constructor(private service: FavoritesService) {
-  }
+  constructor(private service: FavoritesService) {}
+
   @Post('/:userId/:drinkId')
+  @ApiTags('Favorite')
+  @ApiResponse({
+    status: 200,
+    type: Favorite,
+  })
   async toFavorite(
     @Param('userId') userId: number,
     @Param('drinkId') drinkId: number,
