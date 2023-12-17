6ca21fd3cb4e18450a37d2be641cd8a7194e24cc
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-17 14:19:26 -0300

    chore: add the favorite repository as a provider

--- src/data/data.module.ts
+++ src/data/data.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import {DrinksRepository} from "./repositories/drinks.repository";
 import {InfraModule} from "../infra/infra.module";
+import {FavoritesRepository} from "./repositories/favorites.repository";
 
 @Module({
   imports: [InfraModule],
-  providers: [DrinksRepository],
-  exports: [DrinksRepository]
+  providers: [DrinksRepository, FavoritesRepository],
+  exports: [DrinksRepository, FavoritesRepository]
 })
 export class DataModule {}
