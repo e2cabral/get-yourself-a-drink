8cdade2f07c220077014e31e68bb8674dde4538b
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-17 14:32:23 -0300

    refactor: add a constructor to command request

--- src/main/commands/to-favorite/to-favorite.command.request.ts
+++ src/main/commands/to-favorite/to-favorite.command.request.ts
@@ -1,4 +1,11 @@
 export class ToFavoriteCommandRequest {
+  constructor(
+    userId: number,
+    drinkId: number
+  ) {
+    this.userId = userId
+    this.drinkId = drinkId
+  }
   userId: number
   drinkId: number
 }
