b805fee696a4b5f0740fa37b138ac4c56929559a
Edson Cabral Junior <k2.edsonc@grupofleury.com.br>
2023-12-17 14:31:50 -0300

    chore: add the "to favorite" handler to the main module

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -3,9 +3,10 @@ import {CqrsModule, QueryBus} from "@nestjs/cqrs";
 import {GetDrinksHandler} from "./queries/get-drinks/get-drinks.handler";
 import {DataModule} from "../data/data.module";
 import {GetDrinkByIdHandler} from "./queries/get-drink-by-id/get-drink-by-id.handler";
+import {ToFavoriteHandler} from "./commands/to-favorite/to-favorite.handler";
 
 @Module({
   imports: [CqrsModule, DataModule],
-  providers: [GetDrinksHandler, GetDrinkByIdHandler]
+  providers: [GetDrinksHandler, GetDrinkByIdHandler, ToFavoriteHandler]
 })
 export class MainModule {}
